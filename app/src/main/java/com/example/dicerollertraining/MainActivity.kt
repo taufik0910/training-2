4bad1c6f9c2b72eb861fa3e48d241580f0e98ba1
taufik0910 <taufik.cradle@gmail.com>
2022-08-11 02:11:54 +0700

    menambahkan function rollDice di MainActivity.kt, dengan object memasukan method Toast.make

--- app/src/main/java/com/example/dicerollertraining/MainActivity.kt
+++ app/src/main/java/com/example/dicerollertraining/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.dicerollertraining
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val rollButton : Button = findViewById(R.id.roll_button)
+        rollButton.setOnClickListener{rollDice()}
+    }
+
+    private fun rollDice(){
+        Toast.makeText(this,"botton clicked", Toast.LENGTH_SHORT).show()
     }
 }
